4573e17962d92f4bbdb7a1ae21eb88021289b3c0
Jerson03Contreras <jacontrerasl@unitru.edu.pe>
2024-06-08 06:09:04 -0500

    Semana 5 BD

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::view('/', 'home')->name('home');
+
+Route::get('personas', 'App\Http\Controllers\PersonasController@index')->name('personas');
+Route::get('personas/{nPerCodigo}', 'App\Http\Controllers\PersonasController@show')->name('personas.show');
+Route::view('contacto', 'contacto')->name('contacto');
